28c2f8e3a9a8c41824c35eade313d5f317305cef
vishaltophat <geet.tophat@gmail.com>
2022-12-15 17:09:31 +0530

    and

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const dotenv = require('dotenv')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 // Middlewares
-app.use(express.json())
-app.use(cors())
-dotenv.config()
+app.use(express.json());
+app.use(cors());
+dotenv.config();
 
 // Database
-require('./model/database')
+require("./model/database");
 
 // Sending API request to the controllers
-app.use('/',require('./routes/userRoute'))
-app.use('/dashboard',require('./routes/employeeRoute'))
+app.use("/", require("./routes/userRoute"));
+app.use("/dashboard", require("./routes/employeeRoute"));
 
 // PORT Connectivity
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=> console.log(`Server started at ${PORT}`))
+app.listen(PORT, () => console.log(`Server started at ${PORT}`));
