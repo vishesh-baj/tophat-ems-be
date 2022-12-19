64ae796b8332cfdaf13d87257d3c9fa63ffca23f
anshviyogitophat <diptanshu.tophat@gmail.com>
2022-12-19 17:07:21 +0530

    attendence completed

--- controllers/Employee/updateAttendenceControllers.js
+++ controllers/Employee/updateAttendenceControllers.js
@@ -3,9 +3,20 @@ const Attendence = require("../../model/Schema/attendence");
 const updateAttendenceControllers = async (req,res)=>{
     const _id = req.body.attendenceId;
 
-    const data = await Attendence.findByIdAndUpdate(_id,{new:true},req.body)
-
-    console.log(data)
+    const updated = {
+        date:req.body.date,
+        month:req.body.month,
+        year:req.body.year,
+        status:req.body.status
+    }
+    
+    Attendence.findByIdAndUpdate(_id,updated,(err,result)=>{
+        if(err) return console.log(err)
+        res.status(200)
+        return res.json({
+            message:"Attendence updated successfully"
+        })
+    })
 }
 
 module.exports = updateAttendenceControllers;
